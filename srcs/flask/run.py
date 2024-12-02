ed19f462220255beac8fcebabd6b2730bb35fc50
xmatute- <xmatute-@student.42urduliz.com>
2024-12-02 13:53:39 +0100

    toma bea

--- srcs/flask/run.py
+++ srcs/flask/run.py
@@ -6,15 +6,51 @@
 #    By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/20 11:23:53 by xmatute-          #+#    #+#              #
-#    Updated: 2024/11/20 11:43:38 by xmatute-         ###   ########.fr        #
+#    Updated: 2024/11/26 10:30:21 by xmatute-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from app import create_app
+# from app import create_app
 from config import RunConfig as Config
 
-app = create_app()
 
-if __name__ == '__main__':
-    print(f"Running on {Config.HOST}:{Config.PORT}")
+# app = create_app()
+
+# if __name__ == '__main__':
+#     print(f"Running on {Config.HOST}:{Config.PORT}")
+#     app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
+
+# import os
+# from psycopg import connect
+
+# postgres_db = os.getenv('POSTGRES_DB')
+# postgres_user = os.getenv('POSTGRES_USER')
+# postgres_password = os.getenv('POSTGRES_PASSWORD')
+
+
+# try:
+#     conn = connect(dbname=postgres_db, user=postgres_user, password=postgres_password, host=postgres_db)
+#     print(f"Conectandome con la base de datos {postgres_db} como {postgres_user} password {postgres_password} host postgres_db")
+#     # cur = conn.cursor()
+#     # cur.execute("CREATE TABLE test (id serial PRIMARY KEY, cosas varchar);")
+#     # conn.commit()
+#     # print("Table created")
+# except Exception as e:
+#     print(f"Error: {e}")
+
+from flask import Flask
+
+from database import create_tables
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+    return "Hola Bea!"
+    
+if __name__ == "__main__":
+    create_tables()
     app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
+
+
+    
--- srcs/flask/utils/list_routes.py
+++ srcs/flask/utils/list_routes.py
@@ -6,13 +6,13 @@
 #    By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/20 12:45:49 by xmatute-          #+#    #+#              #
-#    Updated: 2024/11/20 12:48:31 by xmatute-         ###   ########.fr        #
+#    Updated: 2024/11/20 19:52:00 by xmatute-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def list_routes(app):
+def list_routes(app, url_prefix: str = ""):
     # Obtener todas las reglas de URL
-    rules = app.url_map.iter_rules()
+    rules = [rule for rule in app.url_map.iter_rules() if rule.rule.startswith(url_prefix)]
     # Generar enlaces HTML para cada ruta
     links = [f'<a href="{rule.rule}">{rule.rule}</a>' for rule in rules]
     # Renderizar la lista de enlaces como HTML
