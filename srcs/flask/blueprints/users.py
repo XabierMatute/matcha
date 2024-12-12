10a61575296b29d780466154c2266b5a54a679b8
xmatute- <xmatute-@student.42urduliz.com>
2024-12-12 16:56:34 +0100

    maybe

--- srcs/flask/blueprints/users.py
+++ srcs/flask/blueprints/users.py
@@ -1,51 +1,62 @@
 from flask import Blueprint, request, jsonify, session, render_template, redirect, url_for, current_app
 from manager.user_manager import register_user, authenticate_user, get_user_details
 from flask_mail import Message
+from config import DEBUG
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
 
 users_bp = Blueprint('users', __name__, url_prefix='/users')
 
-# Función para enviar el correo de verificación
-def send_verification_email(user):
-    """
-    Envía un correo de verificación al usuario después de registrarse.
-    """
-    verification_link = url_for('users.verify_email', token=user.verification_token, _external=True)
+def generate_verification_token(email):
+    app = current_app._get_current_object()
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+def generate_verification_link(email):
+    token = generate_verification_token(email)
+    return url_for('test_user.verify', token=token, _external=True)
+
+def send_verification_email(username: str, email: str):
+    logging.info(f"Sending verification email to {email}")
     
-    
-    msg = Message('Verificación de Correo',
-                  recipients=[user.email])
-    msg.body = f'Por favor, haz clic en el siguiente enlace para verificar tu correo: {verification_link}'
     try:
-        # Usa `current_app` para acceder a la instancia de Mail
+        link = generate_verification_link(email)
+    except Exception as e:
+        logging.error(f"Error generating verification link: {e}")
+        raise Exception("Error generating verification link") from e
+    try:
+        msg = Message(
+            subject="Verify your email",
+            recipients=[email],
+            html=render_template('verification_mail.html', username=username, verification_link=link, email=email)
+        )
         mail = current_app.extensions['mail']
         mail.send(msg)
     except Exception as e:
-        raise ValueError(f"Error al enviar el correo: {str(e)}")
+        logging.error(f"Error sending verification email: {e}")
+        raise Exception("Error sending verification email") from e
 
 @users_bp.route('/register', methods=['GET'])
 def register_form():
     return render_template('register.html')
 
-from config import DEBUG
 
 # Manejo del registro de usuario
 @users_bp.route('/register', methods=['POST'])
 def register_user_route():
-    """
-    Procesa los datos enviados por el formulario de registro.
-    """
-    data = request.form  # Recibe datos del formulario
+    data = request.form
+    logging.info(f"Received registration data: {data}")
     try:
-        user = register_user(data)  # Intenta registrar al usuario
-        send_verification_email(user)  # Enviar correo de verificación
-        return redirect(url_for('users.login_form'))  # Redirige al login
+        register_user(data)
+        logging.info(f"User {data['username']} registered successfully.")
+        send_verification_email(data["username"], data["email"])
+        logging.info(f"Verification email sent to {data['email']}")
+        return render_template('registered.html')
     except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+        return render_template('register.html', error_message=str(e))
     except Exception as e:
-        if DEBUG:
-            error_message = str(e)
-            return jsonify({f"error": error_message}), 500
-        return jsonify({f"error": "Internal Server Error"}), 500
+        return render_template('register.html', error_message=f"Internal Server Error {str(e)}")
+
 
 # Página de inicio de sesión: Muestra el formulario
 @users_bp.route('/login', methods=['GET'])
