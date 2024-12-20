9afb649e79156b74ac7750e8e29bab3327ea4a4d
xmatute- <xmatute-@student.42urduliz.com>
2024-12-20 17:48:42 +0100

    idk

--- srcs/flask/blueprints/users.py
+++ srcs/flask/blueprints/users.py
@@ -1,48 +1,31 @@
-from flask import Blueprint, request, jsonify
-from manager.user_manager import (
-    fetch_user_by_id,
-    fetch_user_by_username,
-    fetch_user_by_email,
-    verify_user,
-    register_new_user,
-    modify_user,
-    remove_user,
-    get_user_details
-)
+from flask import Blueprint, request, jsonify, session, render_template, redirect, url_for, current_app
+from flask_mail import Message
+from itsdangerous import URLSafeTimedSerializer
 import logging
 
-# Configuración del logger
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from manager.user_manager import register_user, authenticate_user, get_user_details
+from config import DEBUG
 
-<<<<<<< HEAD
 
 
 users_bp = Blueprint('users', __name__, url_prefix='/users')
-=======
-users_bp = Blueprint("users", __name__, url_prefix="/users")
->>>>>>> origin/contablas
 
-def success_response(data=None, message="Operation successful"):
-    """Genera una respuesta de éxito consistente."""
-    return {"success": True, "message": message, "data": data}
-
-def error_response(message="Operation failed", status_code=400, details=None):
-    """Genera una respuesta de error consistente."""
-    response = {"success": False, "message": message}
-    if details:
-        response["details"] = details
-    return jsonify(response), status_code
-
-# Ruta para obtener detalles de usuario por ID o nombre de usuario
-@users_bp.route('/details', methods=['GET'])
-def get_user_details_route():
+def generate_verification_token(email):
     """
-    Obtiene detalles de usuario por ID o nombre de usuario.
-    Parámetros de consulta: ?user_id=<id> o ?username=<username>
+    Generates a verification token for the given email.
+
+    Args:
+        email (str): The email address to generate the token for.
+
+    Returns:
+        str: The generated verification token.
     """
-    user_id = request.args.get("user_id", type=int)
-    username = request.args.get("username", type=str)
+    logging.debug(f"Generating verification token for email: {email}")
+    app = current_app._get_current_object()
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    token = serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+    logging.debug(f"Generated token: {token}")
+    return token
 
 def generate_verification_link(email):
     """
@@ -74,19 +57,7 @@ def send_verification_email(username: str, email: str):
     logging.info(f"Sending verification email to {email}")
     
     try:
-        if user_id:
-            user = get_user_details(user_id, require_verified=True)
-        elif username:
-            user = get_user_details(username, require_verified=True)
-        else:
-            return error_response("Either 'user_id' or 'username' must be provided.", 400)
-
-        if not user:
-            return error_response("User not found.", 404)
-        return jsonify(success_response(data=user, message="User details fetched successfully.")), 200
-    except ValueError as ve:
-        logger.error(f"Validation error: {ve}")
-        return error_response(str(ve), 400)
+        link = generate_verification_link(email)
     except Exception as e:
         logging.error(f"Error generating verification link: {e}")
         raise Exception("Error generating verification link") from e
@@ -131,42 +102,37 @@ def register_form():
     """
     logging.debug("Rendering registration form")
     return render_template('register.html')
-        logger.error(f"Error fetching user details: {e}")
-        return error_response("Failed to fetch user details.", 500, details=str(e))
 
-# Ruta para registrar un nuevo usuario
 @users_bp.route('/register', methods=['POST'])
-def register_user():
+def register_user_route():
     """
-    Crea un nuevo usuario.
-    JSON Payload: { "username": "...", "email": "...", "password_hash": "...", "first_name": "...", "last_name": "..." }
+    Handles user registration.
+
+    Returns:
+        str: The rendered template after registration.
     """
-    data = request.json
+    data = request.form
+    logging.info(f"Received registration data: {data}")
     try:
-        username = data.get("username")
-        email = data.get("email")
-        password_hash = data.get("password_hash")
-        first_name = data.get("first_name")
-        last_name = data.get("last_name")
-
-        if not username or not email or not password_hash:
-            return error_response("Username, email, and password_hash are required.", 400)
-
-        new_user = register_new_user(username, email, password_hash, first_name, last_name)
-        return jsonify(success_response(data=new_user, message="User registered successfully.")), 201
-    except ValueError as ve:
-        logger.error(f"Validation error: {ve}")
-        return error_response(str(ve), 400)
+        register_user(data)
+        logging.info(f"User {data['username']} registered successfully.")
+        send_verification_email(data["username"], data["email"])
+        logging.info(f"Verification email sent to {data['email']}")
+        return render_template('registered.html')
+    except ValueError as e:
+        logging.warning(f"ValueError during registration: {e}")
+        return render_template('register.html', error_message=str(e))
     except Exception as e:
-        logger.error(f"Error registering user: {e}")
-        return error_response("Failed to register user.", 500, details=str(e))
+        logging.error(f"Exception during registration: {e}")
+        return render_template('register.html', error_message=f"Internal Server Error {str(e)}")
 
-# Ruta para actualizar un usuario
-@users_bp.route('/update/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
+@users_bp.route('/login', methods=['GET'])
+def login_form():
     """
-    Actualiza la información de un usuario.
-    JSON Payload: { "username": "...", "email": "...", "first_name": "...", "last_name": "..." }
+    Renders the login form.
+
+    Returns:
+        str: The rendered login form template.
     """
     logging.debug("Rendering login form")
     return render_template('login.html')
@@ -291,57 +257,3 @@ def account(username):
             return redirect(url_for('users.account', username=session.get('username')))
     else:
         return redirect('/login')
-    data = request.json
-    try:
-        updated_user = modify_user(
-            user_id,
-            data.get("username"),
-            data.get("email"),
-            data.get("first_name"),
-            data.get("last_name")
-        )
-        return jsonify(success_response(data=updated_user, message="User updated successfully.")), 200
-    except ValueError as ve:
-        logger.error(f"Validation error: {ve}")
-        return error_response(str(ve), 400)
-    except Exception as e:
-        logger.error(f"Error updating user with ID {user_id}: {e}")
-        return error_response("Failed to update user.", 500, details=str(e))
-
-# Ruta para eliminar un usuario
-@users_bp.route('/delete/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    """
-    Elimina un usuario por su ID.
-    """
-    try:
-        deleted_user = remove_user(user_id)
-        return jsonify(success_response(data=deleted_user, message="User deleted successfully.")), 200
-    except ValueError as ve:
-        logger.error(f"Validation error: {ve}")
-        return error_response(str(ve), 400)
-    except Exception as e:
-        logger.error(f"Error deleting user with ID {user_id}: {e}")
-        return error_response("Failed to delete user.", 500, details=str(e))
-
-# Ruta para verificar un usuario por email
-@users_bp.route('/verify', methods=['POST'])
-def verify_user_email():
-    """
-    Verifica un usuario por su email.
-    JSON Payload: { "email": "..." }
-    """
-    data = request.json
-    try:
-        email = data.get("email")
-        if not email:
-            return error_response("Email is required.", 400)
-
-        verified_user = verify_user(email)
-        return jsonify(success_response(data=verified_user, message="User verified successfully.")), 200
-    except ValueError as ve:
-        logger.error(f"Validation error: {ve}")
-        return error_response(str(ve), 400)
-    except Exception as e:
-        logger.error(f"Error verifying user with email '{email}': {e}")
-        return error_response("Failed to verify user.", 500, details=str(e))
