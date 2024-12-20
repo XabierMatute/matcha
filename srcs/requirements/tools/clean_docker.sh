c3af3615413290646ffd5f7e8512afb3e944b764
xmatute- <xmatute-@student.42urduliz.com>
2024-12-20 11:00:39 +0100

    better script

--- srcs/requirements/tools/clean_docker.sh
+++ srcs/requirements/tools/clean_docker.sh
@@ -3,4 +3,3 @@ docker container prune --force
 docker volume prune --force
 docker volume rm $(docker volume ls -q) #puede dar error
 docker rm -f $(docker ps -a -q) # Elimina todos los contenedores?
-@echo "$(RED)clean done...$(WHITE)"
